1a2de0ccad78cee718f982dae465dba8a0e92af6
коля коля <minakov_nikolai@mail.ru>
2021-02-22 15:27:44 +0300

    1

---
+++ Form3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+  public partial class Form3 : Form
+  {
+    List<Button> buttons;
+    public Form3()
+    {
+      InitializeComponent();
+      buttons = new List<Button>();
+      buttons.AddRange(new Button[5] { button1, button2, button3, button4, button5 });
+      button1.Click += button1_Click;
+      button2.Click += button1_Click;
+      button3.Click += button1_Click;
+      button4.Click += button1_Click;
+      button5.Click += button1_Click;
+    }
+
+        void button1_Click(object sender, EventArgs e)
+        {
+      Random rnd = new Random(DateTime.Now.Millisecond);
+      Button btn = buttons[rnd.Next(buttons.Count)];
+      btn.Visible = !btn.Visible;
+    }
+  }
+}
---
+++ Form4.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+  public partial class Form4 : Form
+  {
+    public Form4()
+    {
+      InitializeComponent();
+    }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+      if ((button2.Enabled == false) & (button3.Enabled == false))
+      {
+        button1.Enabled = false;
+      }
+      button2.Enabled = false;
+        }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      if ((button1.Enabled == false) & (button3.Enabled == false))
+      {
+        button2.Enabled = false;
+      }
+      button3.Enabled = false;
+    }
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+      if ((button1.Enabled == false)&(button2.Enabled == false))
+      {
+        button3.Enabled = false;
+      }
+      button1.Enabled = false;
+    }
+
+
+    private void button5_Click(object sender, EventArgs e)
+    {
+      button1.Enabled = true;
+      button2.Enabled = true;
+      button3.Enabled = true;
+    }
+  }
+}
